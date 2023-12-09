3a78abd06be68b3554037426667a722aafe31c1d
Hemanth Krishna <hkpdev008@gmail.com>
2023-12-09 09:45:30 +0530

    feat: shadcn, onboarding ui

---
+++ src/pages/onboarding.tsx
@@ -0,0 +1,14 @@
+import VerifyAadhar from "@/components/onboarding/verifyaadhar";
+import WalletConnect from "@/components/onboarding/walletconnect";
+import WalletSuccess from "@/components/onboarding/walletsuccess";
+
+
+export default function Onboarding() {
+  return (
+    <div>
+      {/* <WalletConnect/> */}
+      {/* <WalletSuccess/> */}
+      <VerifyAadhar/>
+    </div>
+  );
+}
